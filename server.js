797451a6d0cc6fcbdf844e0f6033b9fd0f47aff9
Daniel Bergmann <dba222@outlook.com>
2021-12-13 22:07:02 +0000

    1733

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const articleRouter = require("./routes/articles");
+const app = express();
+
+// turns ejs into html
+app.set("view engine", "ejs");
+
+app.use("/articles", articleRouter);
+
+app.get("/", (req, res) => {
+  const articles = [
+    {
+      title: "test article",
+      createdAt: new Date(),
+      description: "test description",
+    },
+    {
+      title: "test article 2",
+      createdAt: new Date(),
+      description: "test description 2",
+    },
+  ];
+  res.render("articles/index", { articles: articles });
+});
+
+app.listen(5000);
